0fdb2e5ef7b5c58ca678efe3445599f1ed4ff399
lucasolasz <olaszlucas072@gmail.com>
2024-09-18 22:25:29 -0300

    Adicionando projeto existente

---
+++ src/main/java/desafio/btgpactual/orderms/listener/OrderCreatedListener.java
@@ -0,0 +1,10 @@
+package desafio.btgpactual.orderms.listener;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderCreatedListener {
+
+    public void listen(){}
+
+}
