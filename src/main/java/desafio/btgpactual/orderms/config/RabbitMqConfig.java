dd4d4bf9ec5b9076ea4f5795dfba01da1ce1dfe9
lucasolasz <olaszlucas072@gmail.com>
2024-09-19 16:38:45 -0300

    Cria a fila e se conecta, recebendo o payload

--- src/main/java/desafio/btgpactual/orderms/config/RabbitMqConfig.java
+++ src/main/java/desafio/btgpactual/orderms/config/RabbitMqConfig.java
@@ -1,9 +1,31 @@
 package desafio.btgpactual.orderms.config;
 
+import org.springframework.amqp.core.Declarable;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMqConfig {
 
     public static final String ORDER_CREATED_QUEUE = "btg-pactual-order-created";
+
+    /**
+     * Conversor de payload JAVA em JSON
+     * @return payload convertido
+     */
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    /**
+     * Criação da fila
+     * @return A fila definida na constante
+     */
+    @Bean
+    public Declarable orderCreatedQueue(){
+        return new Queue(ORDER_CREATED_QUEUE);
+    }
 }
--- src/main/java/desafio/btgpactual/orderms/listener/OrderCreatedListener.java
+++ src/main/java/desafio/btgpactual/orderms/listener/OrderCreatedListener.java
@@ -1,10 +1,24 @@
 package desafio.btgpactual.orderms.listener;
 
+import desafio.btgpactual.orderms.listener.dto.OrderCreatedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
+
+import static desafio.btgpactual.orderms.config.RabbitMqConfig.ORDER_CREATED_QUEUE;
+
 
 @Component
 public class OrderCreatedListener {
 
-    public void listen(){}
+    private final Logger logger = LoggerFactory.getLogger(OrderCreatedListener.class);
+
+    @RabbitListener(queues = ORDER_CREATED_QUEUE)
+    public void listen(Message<OrderCreatedEvent> message){
+        logger.info("Message consumed: {}", message);
+    }
+
 
 }
