760b510263e10c8d98ca5c2057f76eef2a9f2f1a
lucasolasz <olaszlucas072@gmail.com>
2024-09-20 17:10:41 -0300

    Versão estável da consulta de itens

---
+++ src/main/java/desafio/btgpactual/orderms/controller/OrderController.java
@@ -0,0 +1,44 @@
+package desafio.btgpactual.orderms.controller;
+
+import desafio.btgpactual.orderms.controller.dto.ApiResponse;
+import desafio.btgpactual.orderms.controller.dto.OrderResponse;
+import desafio.btgpactual.orderms.controller.dto.PaginationResponse;
+import desafio.btgpactual.orderms.service.OrderService;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/customers")
+    public ResponseEntity<?> ola(){
+        System.out.println("ola mundo");
+        return ResponseEntity.ok("Encontrou o método");
+    }
+
+    @GetMapping("/customers/{customerid}/orders")
+    public ResponseEntity<ApiResponse<OrderResponse>> listOrders(@PathVariable("customerid") Long customerId,
+                                                                 @RequestParam(name = "page", defaultValue = "0") Integer page,
+                                                                 @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize){
+
+        var pageResponse = orderService.findAllByCustomerId(customerId, PageRequest.of(page, pageSize));
+
+        var totalOnOrders = orderService.findTotalOnOrdersByCustomerId(customerId);
+
+        return ResponseEntity.ok(new ApiResponse<>(
+                Map.of("totalOnOrders", totalOnOrders),
+                pageResponse.getContent(),
+                PaginationResponse.fromPage(pageResponse)
+        ));
+    }
+}
+
--- src/main/java/desafio/btgpactual/orderms/service/OrderService.java
+++ src/main/java/desafio/btgpactual/orderms/service/OrderService.java
@@ -1,21 +1,32 @@
 package desafio.btgpactual.orderms.service;
 
+import desafio.btgpactual.orderms.controller.dto.OrderResponse;
 import desafio.btgpactual.orderms.entity.OrderEntity;
 import desafio.btgpactual.orderms.entity.OrderItem;
 import desafio.btgpactual.orderms.listener.dto.OrderCreatedEvent;
 import desafio.btgpactual.orderms.repository.OrderRepository;
+import org.bson.Document;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
+
 @Service
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final MongoTemplate mongoTemplate;
 
-    public OrderService(OrderRepository orderRepository){
+    public OrderService(OrderRepository orderRepository, MongoTemplate mongoTemplate){
         this.orderRepository = orderRepository;
+        this.mongoTemplate = mongoTemplate;
     }
 
     public void save(OrderCreatedEvent event){
@@ -27,6 +38,22 @@ public class OrderService {
         entity.setTotal(getTotal(event));
 
         orderRepository.save(entity);
+    }
+
+    public Page<OrderResponse> findAllByCustomerId(Long customerId, PageRequest pageRequest){
+        var orders = orderRepository.findAllByCustomerId(customerId, pageRequest);
+        return orders.map(OrderResponse::fromEntity);
+    }
+
+    public BigDecimal findTotalOnOrdersByCustomerId(Long customerId){
+        var aggregations = newAggregation(
+                match(Criteria.where("customerId").is(customerId)),
+                group().sum("total").as("total")
+        );
+
+        var response = mongoTemplate.aggregate(aggregations, "tb_orders", Document.class);
+
+        return new BigDecimal(response.getUniqueMappedResult().get("total").toString());
     }
 
     private BigDecimal getTotal(OrderCreatedEvent event) {
