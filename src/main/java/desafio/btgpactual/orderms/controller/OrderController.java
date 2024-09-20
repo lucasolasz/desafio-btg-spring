8e48678eb19455616cb69312ca4f89751174c515
lucasolasz <olaszlucas072@gmail.com>
2024-09-20 17:37:44 -0300

    Ativação dos CORS

--- src/main/java/desafio/btgpactual/orderms/controller/OrderController.java
+++ src/main/java/desafio/btgpactual/orderms/controller/OrderController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@CrossOrigin(origins = "*") // Permite todas as origens
 @RestController
 public class OrderController {
 
